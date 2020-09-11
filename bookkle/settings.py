33f1705af2e832f5fa2a72e5c43b5cfec8acb9cc
반현랑 <gusfkd2827@gmail.com>
2020-09-12 01:07:41 +0900

    [Fix] 리뷰 등록 수정 - 책 정보 추가 - permission 추가

--- bookkle/settings.py
+++ bookkle/settings.py
@@ -115,7 +115,11 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 4 
+    'PAGE_SIZE': 4,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
 }
 
 AUTHENTICATION_BACKENDS = (
