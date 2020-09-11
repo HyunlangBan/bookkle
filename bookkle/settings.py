6978975223a9cab0b7aa13bde2928932b8419fc1
Hyunlang Ban <gusfkd2827@gmail.com>
2020-09-12 01:46:44 +0900

    Revert "[Fix] 리뷰 등록 수정"

--- bookkle/settings.py
+++ bookkle/settings.py
@@ -115,11 +115,7 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 4,
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-    ],
+    'PAGE_SIZE': 4 
 }
 
 AUTHENTICATION_BACKENDS = (
--- review/views.py
+++ review/views.py
@@ -1,31 +1,26 @@
 from django.db.models import Count
-from rest_framework import viewsets, status, permissions
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.authentication import (
-        SessionAuthentication,
-        TokenAuthentication
-)
+from rest_framework import viewsets, status
 from review.serializers import ReviewListSerializer, ReviewCreateSerializer
 from review.models import Review, Recommand, Book
 from user.models import User
 from rest_framework.response import Response
 import datetime
 
-
 class ReviewViewSet(viewsets.ModelViewSet):
-#### TEST 데이터에 맞추기 위해 잠시 커맨트처리
+#### TEST 데이터에 맞추기 위해 잠시 커맨트처리ㅓ
 #    DAYS = 30
 #    posted_time = datetime.datetime.now() - datetime.timedelta(DAYS)
 #    now = datetime.datetime.now()
 #    queryset = Review.objects.filter(created_at__range=[posted_time, now]).order_by('-recommand_count')
-    permission_classes = [IsAuthenticatedOrReadOnly]
+
     queryset = Review.objects.order_by('-recommand_count')
     serializer_class = ReviewListSerializer
 
     def create(self, request, *args, **kwargs):
-        data = request.data
-        serializer_class = ReviewCreateSerializer(data=data)
-        serializer_class.is_valid()
-        book = Book.objects.get_or_create(title=data['book_title'], author=data['book_author'], image=data['book_image'])[0]
-        Review.objects.create(book=book, title = data['title'], content = data['content'], rating = data['rating'], quote = data['quote'], user=request.user)
-        return Response(serializer_class.data)
+        serializer_class = ReviewCreateSerializer
+        ######## user 추가 필요
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
