ba42a818b5ba1c28ce107bad61f0d781d42b139e
반현랑 <gusfkd2827@gmail.com>
2020-09-12 01:07:41 +0900

    [Fix] 리뷰 등록 수정 - 책 정보 추가 - permission 추가

--- review/serializers.py
+++ review/serializers.py
@@ -21,17 +21,21 @@ class BookSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class ReviewCreateSerializer(serializers.ModelSerializer):
+    book_title = serializers.CharField() 
+    book_author = serializers.CharField()
+    book_image = serializers.URLField()
+
     class Meta:
         model = Review
         exclude = ['recommand_count']
 
 class ReviewListSerializer(serializers.ModelSerializer):
-    book_detail = BookSerializer(source='book', read_only = True)
+    book_detail = BookSerializer(source='book')
     user_info = UserProfileSerializer(source='user', read_only = True)
 
     class Meta:
         model = Review
-        fields = ['id','title', 'book', 'user', 'content', 'rating','quote', 'recommand_count', 'book_detail', 'user_info']
+        fields = ['id', 'title', 'content', 'rating','quote', 'recommand_count', 'book_detail', 'user_info']
         read_only_fields = ['recommand_count']
 
 
