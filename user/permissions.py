da7741b530cc28585c9a3116eb8cb453e0a3d8d4
반현랑 <gusfkd2827@gmail.com>
2020-09-14 18:31:09 +0900

    [Add] 유저별 작성 리뷰 모아보기 View 추가 - MyReview View 추가 - ReviewViewSet permission 수정 - 페이지네이션 추가

---
+++ user/permissions.py
@@ -0,0 +1,16 @@
+from rest_framework import permissions
+
+class IsReviewAuthorOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(
+            request.method in permissions.SAFE_METHODS or
+            request.user and
+            request.user.is_authenticated
+        )
+    def has_object_permission(self, request, view, obj):
+        return bool(
+            request.method in permissions.SAFE_METHODS or
+            (request.user.is_authenticated and
+            obj.user == request.user)
+        )
+
